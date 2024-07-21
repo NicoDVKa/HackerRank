135daf14ddab8a9c29bfb3c2115567af7ff0428e
NicoDVKa <herlannicolas6@gmail.com>
2024-07-21 15:57:51 -0300

    A very big sum

---
+++ HackerRank/A-Very-Big-Sum/AVeryBigSum.cs
@@ -0,0 +1,20 @@
+﻿
+namespace HackerRank.A_Very_Big_Sum
+{
+    public class AVeryBigSum
+    {
+        public static long aVeryBigSum(List<long> ar)
+        {
+            long result = 0;
+
+            foreach (long v in ar) result += v;
+ 
+            return result;
+        }
+
+        public static long aVeryBigSumV1(List<long> ar)
+        {
+            return ar.Sum(x => x);
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using HackerRank.A_Very_Big_Sum;
+
+var arr = new List<long>() { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+
+var result = AVeryBigSum.aVeryBigSum(arr);
+
+Console.WriteLine(result);
