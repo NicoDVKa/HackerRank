620745d2076b1498963367fc89314b064a9985e6
NicoDVKa <herlannicolas6@gmail.com>
2024-07-21 17:25:08 -0300

    AVeryBigSum: add a printer

---
+++ HackerRank/A-Very-Big-Sum/PrintAVeryBigSum.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HackerRank.A_Very_Big_Sum
+{
+    public class PrintAVeryBigSum
+    {
+        public static void Print()
+        {
+            var arr = new List<long>() { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+
+            var result = AVeryBigSum.aVeryBigSum(arr);
+
+            Console.WriteLine(result);
+        }
+    }
+}
