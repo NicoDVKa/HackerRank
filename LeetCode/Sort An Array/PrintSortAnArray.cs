75b1c96db00cbea4deb95f03bb2628573fc80479
NicoDVKa <herlannicolas6@gmail.com>
2024-07-25 22:52:13 -0300

    Add Sort An Array Solution (Merge Sort)

--- LeetCode/Sort An Array/PrintSortAnArray.cs
+++ LeetCode/Sort An Array/PrintSortAnArray.cs
@@ -4,5 +4,16 @@ namespace LeetCode.Sort_An_Array
 {
     public class PrintSortAnArray
     {
+     
+
+        //SortAnArray.Execute(array).ForEach(x => Console.Write(x + " "));
+
+        public static void Print()
+        {
+
+            List<int> array = [-1, 5, 1, int.MaxValue, 1, 2, 0, 0];
+
+            SortAnArray.Execute(array).ForEach(x => Console.Write(x + " "));
+        }
     }
 }
--- LeetCode/Sort An Array/SortAnArray.cs
+++ LeetCode/Sort An Array/SortAnArray.cs
@@ -4,6 +4,49 @@ namespace LeetCode.Sort_An_Array
 {
     public class SortAnArray
     {
+        public static List<int> Execute(List<int> arrayToSort)
+        {
+            return MergeSort(arrayToSort);
+        }
 
+        static List<int> MergeSort(List<int> arr)
+        {
+            if (arr.Count == 1) return arr;
+
+            int middle = arr.Count / 2;
+            List<int> leftArray = arr.Take(middle).ToList();
+            List<int> rightArray = arr.Skip(middle).ToList();
+
+            List<int> sortedLeftArray = MergeSort(leftArray);
+            List<int> sortedRightArray = MergeSort(rightArray);
+
+            return Merge(sortedLeftArray, sortedRightArray);
+        }
+
+        static List<int> Merge(List<int> leftArray, List<int> rightArray)
+        {
+            List<int> resultArray = [];
+
+            while (leftArray.Count > 0 && rightArray.Count > 0)
+            {
+                if (leftArray[0] < rightArray[0])
+                {
+                    resultArray.Add(leftArray[0]);
+                    leftArray.RemoveAt(0);
+                }
+                else
+                {
+                    resultArray.Add(rightArray[0]);
+                    rightArray.RemoveAt(0);
+                }
+            }
+            if (leftArray.Count > 0)
+                resultArray.AddRange(leftArray);
+
+            if (rightArray.Count > 0)
+                resultArray.AddRange(rightArray);
+
+            return resultArray;
+        }
     }
 }
